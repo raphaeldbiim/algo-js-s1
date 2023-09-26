ed334320b91e98e8091ae98a7308b4f47bddcb95
raphaeldbiim <146094444+raphaeldbiim@users.noreply.github.com>
2023-09-26 17:47:18 +0200

    adding exercice1 and exercice2

---
+++ Exercices/main.js
@@ -0,0 +1,8 @@
+a = 4
+b = 4
+c = 3
+if (a==b){console.log("c'est égal")}
+if(c<b && a+c !=3){
+    console.log("ok")
+}
+else {console.log("raté")}
---
+++ learning/main.js
@@ -0,0 +1,13 @@
+alert ("bonjour, tu veux quitter ce site ?")
+
+console.log("bienvenue dans la console")
+alert("je suis bien lié à l'index.html")
+var cardname = "choixpeau"
+const teacherage = 28
+const studentage =17
+if (6>7) {
+    consolelog(dgzduz)
+}
+else{
+    consolelog(hfuegfug)
+}
